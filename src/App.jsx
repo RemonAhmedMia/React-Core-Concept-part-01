bf14f8b176b014ff6cedc400074aad23da149c7b
TheRemonDev <riyanahmed3536@gmail.com>
2025-10-17 15:55:54 +0100

    props-part-01

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,19 @@ function App() {
 
   return (
     <>
-    <h1 className='text-red-800'>Hello</h1>
+    <Player name = 'Tamim Iqbal' runs= '5000'></Player>
+    <Player name = 'Shakib Al-Hasan' runs= '6000'></Player>
+    
     </>
+  )
+}
+
+function Player(props) {
+  return(
+    <div>
+      <h3>Player: {props.name}</h3>
+      <p>Runs: {props.runs}</p>
+    </div>
   )
 }
 
