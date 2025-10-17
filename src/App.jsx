3a489b3e7969eb8e41de526732cafd010fbf2fc8
TheRemonDev <riyanahmed3536@gmail.com>
2025-10-17 16:09:34 +0100

    destrucrting

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
     <Player name = 'Tamim Iqbal' runs= '5000'></Player>
     <Player name = 'Shakib Al-Hasan' runs= '6000'></Player>
-    
+    <PlayerOne name={'Rimo'} runs={'98456986'}></PlayerOne>
     </>
   )
 }
@@ -18,6 +18,17 @@ function Player(props) {
     <div>
       <h3>Player: {props.name}</h3>
       <p>Runs: {props.runs}</p>
+    </div>
+  )
+}
+
+// Destructuring====
+// const {name,runs} = {name: "Tamim Hasan", runs: "98598"}
+function PlayerOne({name,runs}){
+  return(
+    <div>
+      <h2>Name: {name}</h2>
+      <p>Runs: {runs}</p>
     </div>
   )
 }
